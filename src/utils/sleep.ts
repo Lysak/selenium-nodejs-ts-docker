31590e6632493e1d5b986ef99971f544a7af3e30
Dmytrii Lysak <dlysak@live.com>
2025-06-14 21:23:18 +0300

    update code #2

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,3 +1,13 @@
-export function sleep(ms: number): Promise<void> {
-    return new Promise(res => setTimeout(res, ms));
-} 
+export function randomDelay(intMin: number = 50, intMax: number = 150) {
+    return Math.floor(Math.random() * (intMax - intMin + 1) + intMin);
+}
+
+export function sleep(ms: number = 1000, intMin: number = 500, intMax: number = 1500): Promise<void> {
+    const msRandomDelay = randomDelay(intMin, intMax);
+
+    const totalDelay = Math.floor(ms) + msRandomDelay;
+
+    // console.log(`Sleeping for ${totalDelay}ms (base: ${ms}, random: ${msRandomDelay})`, `"lysak"`);
+
+    return new Promise(res => setTimeout(res, totalDelay));
+}
